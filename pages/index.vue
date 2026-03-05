cf5498d2c839b139487de48447eaa1a95c6b54e7
Jostrod <jostroda@stud.ntnu.no>
2026-03-05 19:53:41 +0100

    Start creating navBar

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,24 @@
 <template>
     <div>
+
+        <header>
+            <nav>
+                <ul>
+                    <li><NuxtLink to="/">Home</NuxtLink></li>
+                    <li><NuxtLink to="/about">About</NuxtLink></li>
+                    <li><NuxtLink to="/products">products</NuxtLink></li>
+
+                </ul>
+            </nav>
+        </header>
         <h2>Home</h2>
+
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis hic corrupti quibusdam. 
             Nihil, doloribus dolorum. Saepe sapiente dicta modi molestias alias, 
-            voluptatem impedit nulla deserunt tempore possimus doloremque tempora aut!
-        </p>
-
+            voluptatem impedit nulla deserunt tempore possimus doloremque tempora aut!</p>
          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis hic corrupti quibusdam. 
             Nihil, doloribus dolorum. Saepe sapiente dicta modi molestias alias, 
-            voluptatem impedit nulla deserunt tempore possimus doloremque tempora aut!
-        </p>
+            voluptatem impedit nulla deserunt tempore possimus doloremque tempora aut!</p>
 
     </div>
 </template>
