89047a9b3111066140cb3badac61c41eb0e6c21c
Jostrod <jostroda@stud.ntnu.no>
2026-03-05 19:44:24 +0100

    Create index, about and [id].vue

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis hic corrupti quibusdam. 
+            Nihil, doloribus dolorum. Saepe sapiente dicta modi molestias alias, 
+            voluptatem impedit nulla deserunt tempore possimus doloremque tempora aut!
+        </p>
+
+         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis hic corrupti quibusdam. 
+            Nihil, doloribus dolorum. Saepe sapiente dicta modi molestias alias, 
+            voluptatem impedit nulla deserunt tempore possimus doloremque tempora aut!
+        </p>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+h2{
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p {
+    margin: 20px 0;
+}
+
+</style>
