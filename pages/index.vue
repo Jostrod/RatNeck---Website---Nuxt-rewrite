6f8e8dfc32c2b4dac4a39a3da8b39b6b52f6d5fa
Jostrod <jostroda@stud.ntnu.no>
2026-03-27 19:27:38 +0100

    Delete now redundant files

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,6 @@
 <template>
     <div>
-
-        <header>
-            <nav>
-                <ul>
-                    <li><NuxtLink to="/">Home</NuxtLink></li>
-                    <li><NuxtLink to="/about">About</NuxtLink></li>
-                    <li><NuxtLink to="/products">products</NuxtLink></li>
-
-                </ul>
-            </nav>
-        </header>
-        <h2>Home</h2>
-
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis hic corrupti quibusdam. 
-            Nihil, doloribus dolorum. Saepe sapiente dicta modi molestias alias, 
-            voluptatem impedit nulla deserunt tempore possimus doloremque tempora aut!</p>
-         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis hic corrupti quibusdam. 
-            Nihil, doloribus dolorum. Saepe sapiente dicta modi molestias alias, 
-            voluptatem impedit nulla deserunt tempore possimus doloremque tempora aut!</p>
-
+        <p>Index</p>
     </div>
 </template>
 
@@ -28,14 +9,5 @@
 </script>
 
 <style scoped>
-
-h2{
-    margin-bottom: 20px;
-    font-size: 36px;
-}
-
-p {
-    margin: 20px 0;
-}
 
 </style>
